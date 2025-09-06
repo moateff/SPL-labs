88c0a41fbb0c4f534977cf94086a8193576329e5
moateff <mohamed.atef.yousef46@gmail.com>
2025-09-06 03:48:42 +0300

    Commit echo.c

--- 02-echo/echo.c
+++ 02-echo/echo.c
@@ -1,7 +1,32 @@
-// Add the needed includes
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
-int echo_main(int argc, char *argv[]) {
-    // Write your code here
-    
+int echo_main(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; i++) {
+        size_t len = strlen(argv[i]);
+        ssize_t ret = write(STDOUT_FILENO, argv[i], len);
+        if (ret != (ssize_t) len) {
+            perror("write");
+            return 1;       
+        }
+
+        if (i < argc - 1) {
+            ret = write(STDOUT_FILENO, " ", 1);
+            if (ret != 1) {
+                perror("write");
+                return 1;
+            }
+        }
+    }
+
+    ssize_t ret = write(STDOUT_FILENO, "\n", 1);
+    if (ret != 1) {
+        perror("write");
+        return 1;
+    }
+
     return 0;
 }
