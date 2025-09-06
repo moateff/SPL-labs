79a072b03a567bdc560e38647e5a20a5f5692c7d
moateff <mohamed.atef.yousef46@gmail.com>
2025-09-06 03:32:57 +0300

    Commit pwd

--- 01-pwd/pwd.c
+++ 01-pwd/pwd.c
@@ -1,7 +1,32 @@
-// Add the needed includes
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
 
-int pwd_main() {
-    // Write your code here
-    
-    return 0;
+#define PATH_MAX 4096
+
+int pwd_main()
+{
+    char cwd[PATH_MAX];
+
+    // Get current working directory
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        ssize_t ret = write(STDOUT_FILENO, cwd, strlen(cwd));
+        if (ret < 0) {
+            perror("write");
+            return 1;
+        }
+
+        ret = write(STDOUT_FILENO, "\n", 1);
+        if (ret < 0) {
+            perror("write");
+            return 1;
+        }
+
+        return 0;
+    } else {
+        perror("getcwd");
+        return 1;
+    }
 }
