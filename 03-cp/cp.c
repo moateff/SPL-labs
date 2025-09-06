e7ce2cf1084d7540391bffa5f683bac90caff8b2
moateff <mohamed.atef.yousef46@gmail.com>
2025-09-06 03:53:13 +0300

    Commit cp.c

--- 03-cp/cp.c
+++ 03-cp/cp.c
@@ -1,7 +1,47 @@
-// Add the needed includes
+#include <fcntl.h>
+#include <unistd.h>
 
-int cp_main(int argc, char *argv[]) {
-    // Write your code here
+#define BUFSIZE 1024
 
+int cp_main(int argc, char *argv[])
+{
+
+    if (argc != 3) {
+        write(STDERR_FILENO, "Usage: cp <source> <destination>\n", 34);
+        return 1;
+    }
+
+    int fd_src, fd_dest;
+    char buffer[BUFSIZE];
+    ssize_t bytes_read, bytes_written;
+
+    fd_src = open(argv[1], O_RDONLY);
+    if (fd_src < 0) {
+        return 1;
+    }
+
+    fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd_dest < 0) {
+        close(fd_src);
+        return 1;
+    }
+
+    while ((bytes_read = read(fd_src, buffer, sizeof(buffer))) > 0) {
+        bytes_written = write(fd_dest, buffer, bytes_read);
+        if (bytes_written != bytes_read) {
+            close(fd_src);
+            close(fd_dest);
+            return 1;
+        }
+    }
+
+    if (bytes_read < 0) {
+        close(fd_src);
+        close(fd_dest);
+        return 1;
+    }
+
+    close(fd_src);
+    close(fd_dest);
     return 0;
 }
